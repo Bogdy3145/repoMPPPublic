4f18e3f7bed77ff109863082bfed154075c2e144
Bogdy3145 <71905083+Bogdy3145@users.noreply.github.com>
2023-03-27 01:57:16 +0300

    Update settings.py

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 ]
-
+#da
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware"
     "django.middleware.security.SecurityMiddleware",
